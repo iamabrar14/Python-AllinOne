ce6472112402ba3bdaa69573c87732cb81ebaa44
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2025-07-29 01:00:06 +0600

    Add files via upload

---
+++ SpecialArrays.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+print("Zeros\n : ",np.zeros((2,3))) #2*3 matrix with all zeros
+print("Ones\n : ",np.ones((2,2)))  #2*2 matrix with all ones
+print("Range\n : ",np.arange(0,10,2))
+print("Linspace\n :",np.linspace(0,1,5))

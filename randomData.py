aee1632d64edc56eb9c3dc6feb6e663c8c6051b0
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2025-08-03 02:12:08 +0600

    Add files via upload

---
+++ randomData.py
@@ -0,0 +1,10 @@
+import numpy as np
+data = np.random.randint(10, 100, size=20)
+
+print("Data:", data)
+print("Mean:", np.mean(data))
+print("Median:", np.median(data))
+print("Standard Deviation:", np.std(data))
+print("Minimum:", np.min(data))
+print("Maximum:", np.max(data))
+

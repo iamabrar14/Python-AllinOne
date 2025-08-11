b92bcee3bd488c50904de2beea52570f60c82cd4
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2025-08-11 22:38:42 +0600

    Add files via upload

---
+++ tuple1.py
@@ -0,0 +1,7 @@
+#Tuple means immutable sequence of elements, Defined using parenthesis
+#Can contain mixed data types
+#Mutability refers to wheater an object can be changed or not after its declaration
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+print(my_tuple[2])
+print(my_tuple[3])
